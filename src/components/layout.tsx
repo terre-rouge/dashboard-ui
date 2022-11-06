b0746bcd22372b075e3acbaebc5f799944959034
laurentDellaNegra <laurent.dellanegra@gmail.com>
2022-11-06 20:05:16 +0100

    🤡 Add fake home dashboard

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -245,13 +245,7 @@ export default function Layout({ title, user, status, children }: Props) {
               <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
             </div>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-              {/* Replace with your content */}
-              <div className="py-4">
-                <div className="h-96 rounded-lg border-4 border-dashed border-gray-200">
-                  {children}
-                </div>
-              </div>
-              {/* /End replace */}
+              <div className="py-4">{children}</div>
             </div>
           </div>
         </main>
